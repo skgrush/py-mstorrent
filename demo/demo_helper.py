4f28660546a8db44ee8b84ee65598f2cbabce01a
Samuel K. Grush <samuel@grush.org>
2016-12-06 02:53:37 -0600

    Added timeout at 120s for peers 1 and 2

--- demo/demo_helper.py
+++ demo/demo_helper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """py-mstorrent Demo Helper Script"""
 
+import signal
 import time
 import sys
 import os
@@ -48,3 +49,14 @@ def waiter(waitTil, inc=5):
             time.sleep(1)
             continue
 
+class timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
